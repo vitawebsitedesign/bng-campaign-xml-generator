3a9b1924e9db1c4cf7eb7d2c114c8df76c1c4ed7
Michael Nguyen <3821810+vitawebsitedesign@users.noreply.github.com>
2018-12-22 15:18:40 +1100

    Update and rename campaign-list.json to campaign-list.js

---
+++ campaign-list.js
@@ -0,0 +1,37 @@
+class CampaignList {
+	get() {
+		return [{
+				"name": "New",
+				"url": "https://gist.githubusercontent.com/vitawebsitedesign/59a3a0c815c0fcfa07e6ae2c664464a9/raw/new-campaign.xml"
+			},
+			{
+				"name": "BallisticNG legacy",
+				"url": ""
+			},
+			{
+				"name": "Enai Siaion",
+				"url": ""
+			},
+			{
+				"name": "Kyra/Evy/Victor/Shroom",
+				"url": ""
+			},
+			{
+				"name": "Natural landscapes",
+				"url": ""
+			},
+			{
+				"name": "Wipeout64 race challenges",
+				"url": "https://gist.githubusercontent.com/vitawebsitedesign/7ca54549568127dd3f9e2348d3ac8781/raw/wipeout-64-race-challenges.xml"
+			},
+			{
+				"name": "Wipeout64 time trial challenges",
+				"url": "https://gist.githubusercontent.com/vitawebsitedesign/9c5ba20e16ec4dcf54a1477616b22af5/raw/wipeout-64-time-trial-challenges.xml"
+			},
+			{
+				"name": "Wipeout64 elimination challenges",
+				"url": "https://gist.githubusercontent.com/vitawebsitedesign/631872cfbba2b2d05a9325023dbbe23a/raw/wipeout-64-eliminator-challenges.xml"
+			}
+		];	
+	}
+}
