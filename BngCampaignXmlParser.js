7e28e102085d7ac75451d3adad4fca9910f9d92a
Michael Nguyen <3821810+vitawebsitedesign@users.noreply.github.com>
2018-12-22 15:13:38 +1100

    Update BngCampaignXmlParser.js

--- BngCampaignXmlParser.js
+++ BngCampaignXmlParser.js
@@ -1,88 +1,98 @@
-class BngCampaignXmlParser {
-    convertBngCampaignXmlToXmlDoc(str)
-    {
-        const parser = new DOMParser();
-        const wellFormedXmlStr = `<root>${str}</root>`;
-        return parser.parseFromString(wellFormedXmlStr, 'text/xml');
-    }
+  class BngCampaignXmlParser {
+      convertBngCampaignXmlToXmlDoc(str) {
+          const parser = new DOMParser();
+          const wellFormedXmlStr = `<root>${str}</root>`;
+          return parser.parseFromString(wellFormedXmlStr, 'text/xml');
+      }
 
-    setHtmlFieldsForXml(mappings, xml) {
-            // Settings tag
-            const settings = this.getSelectorValuesMapForTagName(mappings, xml, 'Settings');
-            console.log(settings);    // dm
-                // Set html field vals normally
+      setHtmlFieldsForXml(mappings, xml) {
+        // Settings tag
+        const settings = this.getSelectorValuesMapForTagName(mappings, xml, 'Settings');
+        this.setHtmlFieldsForMap('.campaign-options-container', settings);
+        const campaignName = settings.filter(m => m.selector.toLowerCase() === '#campaign-name')[0].val;
+        setCampaignFilename(slugify(campaignName));
 
-            // For ea group
-            const groupTags = xml.getElementsByTagName('Group');
-            for (let groupTag of groupTags) {
-                // addGroup();
-                // Apply "PointsToUnlock"
-                const group = this.getSelectorValuesMapForTag(mappings, xml, 'Group', groupTag);
-                console.log(group);    // dm
-                // For ea event childnode
-                const eventTags = groupTag.getElementsByTagName('Event');
-                for (let eventTag of eventTags) {
-                    // Apply frontend
-                    const frontendTag = eventTag.getElementsByTagName('Frontend')[0];
-                    const frontend = this.getSelectorValuesMapForTag(mappings, xml, 'Frontend', frontendTag);
-                    console.log(frontend);    // dm
-                    // Apply awards
-                    const awardsTag = eventTag.getElementsByTagName('Awards')[0];
-                    const awards = this.getSelectorValuesMapForTag(mappings, xml, 'Awards', awardsTag);
-                    console.log(awards);    // dm
-                    // addEvent();
-                    // Apply Mode/Modifiers/Ai
-                    const eventSettingsTag = groupTag.getElementsByTagName('EventSettings')[0];
+        // For ea group
+        const groupTags = xml.getElementsByTagName('Group');
+        for (let groupTag of groupTags) {
+          addGroup(false, false);
+          // Apply "PointsToUnlock"
+          const group = this.getSelectorValuesMapForTag(mappings, xml, 'Group', groupTag);
+          this.setHtmlFieldsForMap('.group', group);
+          // For ea event childnode
+          const eventTags = groupTag.getElementsByTagName('Event');
+          for (let eventTag of eventTags) {
+            const latestGroupEle = $('.campaign-container .group').last();
+            addEvent(false, latestGroupEle);
 
-                    const modeTag = eventSettingsTag.getElementsByTagName('Mode')[0];
-                    const modifiersTag = eventSettingsTag.getElementsByTagName('Modifiers')[0];
-                    const aiTag = eventSettingsTag.getElementsByTagName('Ai')[0];
+            // Apply frontend
+            const frontendTag = eventTag.getElementsByTagName('Frontend')[0];
+            const frontend = this.getSelectorValuesMapForTag(mappings, xml, 'Frontend', frontendTag);
+            this.setHtmlFieldsForMap('.event', frontend);
+            // Apply awards
+            const awardsTag = eventTag.getElementsByTagName('Awards')[0];
+            const awards = this.getSelectorValuesMapForTag(mappings, xml, 'Awards', awardsTag);
+            this.setHtmlFieldsForMap('.event', awards);
+            // Apply Mode/Modifiers/Ai
+            const eventSettingsTag = groupTag.getElementsByTagName('EventSettings')[0];
 
-                    const mode = this.getSelectorValuesMapForTag(mappings, xml, 'Mode', modeTag);
-                    const modifiers = this.getSelectorValuesMapForTag(mappings, xml, 'Modifiers', modifiersTag);
-                    const ai = this.getSelectorValuesMapForTag(mappings, xml, 'Ai', aiTag);
+            const modeTag = eventSettingsTag.getElementsByTagName('Mode')[0];
+            const modifiersTag = eventSettingsTag.getElementsByTagName('Modifiers')[0];
+            const aiTag = eventSettingsTag.getElementsByTagName('Ai')[0];
 
-                    console.log(mode);
-                    console.log(modifiers);
-                    console.log(ai);
-                    // For ea Level childnode
-                    const levelTags = eventSettingsTag.getElementsByTagName('Level');
-                    for (let levelTag of levelTags) {
-                        // addLevel();
-                        // Apply level name
-                        const level = this.getSelectorValuesMapForTag(mappings, xml, 'Level', levelTag);
-                        console.log(level);
-                    }
-                }
+            const mode = this.getSelectorValuesMapForTag(mappings, xml, 'Mode', modeTag);
+            const modifiers = this.getSelectorValuesMapForTag(mappings, xml, 'Modifiers', modifiersTag);
+            const ai = this.getSelectorValuesMapForTag(mappings, xml, 'Ai', aiTag);
+            this.setHtmlFieldsForMap('.event', mode);
+            this.setHtmlFieldsForMap('.event', modifiers);
+            this.setHtmlFieldsForMap('.event', ai);
+            
+            // For ea Level childnode
+            const levelTags = eventSettingsTag.getElementsByTagName('Level');
+            for (let levelTag of levelTags) {
+              const addLevelBtnEle = $('.campaign-container .btn-add-level').last();
+              addLevel(false, addLevelBtnEle);
+              // Apply level name
+              const level = this.getSelectorValuesMapForTag(mappings, xml, 'Level', levelTag);
+              this.setHtmlFieldsForMap('.event', level);
             }
-    }
-
-    getSelectorValuesMapForTagName(mappings, xml, tagName) {
-        return mappings.filter(m => m.xml.tag.toLowerCase() === tagName.toLowerCase())
-            .map(m => ({
-                selector: m.html.selector,
-                val: this.getXmlAttr(xml, m.xml.tag, m.xml.attr)
-            }));
-    }
-
-    getSelectorValuesMapForTag(mappings, xml, tagName, tag) {
-        return mappings.filter(m => m.xml.tag.toLowerCase() === tagName.toLowerCase())
-            .map(m => ({
-                selector: m.html.selector,
-                val: tag.getAttribute(m.xml.attr)
-            }));
-    }
-
-    getXmlAttr(xml, tagName, attrName) {
-        const tag = xml.getElementsByTagName(tagName)[0];
-        if (tag) {
-            const attrVal = tag.getAttribute(attrName);
-            if (attrVal !== undefined) {
-                return attrVal;
-            }
+          }
         }
+      }
 
-        console.warn(`Tag "${tagName}" with attribute "${attrName}" not found in xml`);
-        return null;
-    }
-}
+      setHtmlFieldsForMap(parentSelector, mappingsForXmlTag) {
+        for (let mapping of mappingsForXmlTag) {
+          const parent = $(`.campaign-container ${parentSelector}`).last();
+          parent.find(mapping.selector).val(mapping.val);
+        }
+      }
+
+      getSelectorValuesMapForTagName(mappings, xml, tagName) {
+          return mappings.filter(m => m.xml.tag.toLowerCase() === tagName.toLowerCase())
+              .map(m => ({
+                  selector: m.html.selector,
+                  val: this.getXmlAttr(xml, m.xml.tag, m.xml.attr)
+              }));
+      }
+
+      getSelectorValuesMapForTag(mappings, xml, tagName, tag) {
+          return mappings.filter(m => m.xml.tag.toLowerCase() === tagName.toLowerCase())
+              .map(m => ({
+                  selector: m.html.selector,
+                  val: tag.getAttribute(m.xml.attr)
+              }));
+      }
+
+      getXmlAttr(xml, tagName, attrName) {
+          const tag = xml.getElementsByTagName(tagName)[0];
+          if (tag) {
+              const attrVal = tag.getAttribute(attrName);
+              if (attrVal !== undefined) {
+                  return attrVal;
+              }
+          }
+
+          console.warn(`Tag "${tagName}" with attribute "${attrName}" not found in xml`);
+          return null;
+      }
+  }
