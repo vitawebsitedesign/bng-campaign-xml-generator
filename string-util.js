7dbf7e79413f8550ee70862a402bce98eabb769e
Michael Nguyen <3821810+vitawebsitedesign@users.noreply.github.com>
2018-12-22 16:29:17 +1100

    Create string-util.js

---
+++ string-util.js
@@ -0,0 +1,37 @@
+class StringUtil {
+  static randomizeWebsiteColour() {
+    const btnColour = this.getRandomColour();
+    const headerGradient = `linear-gradient(45deg, #043751 0%, #104f71 30%, ${btnColour} 80%, ${btnColour} 100%)`;
+    $('button').css({'background-color': btnColour, 'border-color': btnColour});
+    $('header').css('background', headerGradient);
+  }
+
+  static getRandomColour() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+  static slugify(str) {
+    return str.toLowerCase().replace(/ /gi, '-');
+  }
+
+  static getCustomCampaignFilePath(campaignName) {
+    const folder = `C:\\...\\BallisticNG\\UserData\\Custom Campaigns`;
+    const fileName = this.slugify(campaignName);
+    const fileWithExtension = `${slugifiedCampaignName}.xml`;
+    const path = `${folder}\\${fileWithExtension}`;
+  }
+
+  static capitaliseFirstLetter(bool) {
+    let str = bool.toString();
+    return this.replaceAt(str, 0, str[0].toUpperCase());
+  }
+
+  static replaceAt(str, index, replacement) {
+    return str.substr(0, index) + replacement + str.substr(index + replacement.length);
+  }
+}
