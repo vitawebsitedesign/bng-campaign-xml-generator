b8197b02a6efe738688e90564decea13d5934498
Michael Nguyen <3821810+vitawebsitedesign@users.noreply.github.com>
2018-12-28 20:10:26 +1100

    consistent spacing

--- src/js/classes/campaign-list.js
+++ src/js/classes/campaign-list.js
@@ -1,7 +1,7 @@
 class CampaignList {
 	static get() {
 		return [
-      			{
+			{
 				"name": "new",
 				"url": "https://gist.githubusercontent.com/vitawebsitedesign/59a3a0c815c0fcfa07e6ae2c664464a9/raw/new-campaign.xml"
 			},
@@ -25,7 +25,7 @@ class CampaignList {
 				"name": "wipeout64 time trial challenges",
 				"url": "https://gist.githubusercontent.com/vitawebsitedesign/9c5ba20e16ec4dcf54a1477616b22af5/raw/wipeout-64-time-trial-challenges.xml"
 			}
-		];	
+		];
 	}
 }
 
--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 const webdriver = require('selenium-webdriver');
-const browser = new webdriver.Builder().usingServer().withCapabilities({'browserName': 'firefox' }).build();
+const browser = new webdriver.Builder().usingServer().withCapabilities({ 'browserName': 'firefox' }).build();
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
 const expect = chai.expect;
@@ -13,44 +13,44 @@ const targetHtmlUri = `file:///${targetHtmlPath}`;
 chai.use(chaiAsPromised);
 chai.should();
 
-describe('bng campaign xml generator', function() {
+describe('bng campaign xml generator', function () {
   this.timeout(100000);
 
-  describe('loading templates', function() {
+  describe('loading templates', function () {
     beforeEach(() => browser.get(targetHtmlUri));
 
     it('should be able to load "New" template', async () => {
       const xml = await canLoadXmlFromRemoteTemplatePromise('new');
       expect(xml.length).to.equal(116);
     });
-  
+
     it('should be able to load "BNGT" template', async () => {
       const xml = await canLoadXmlFromRemoteTemplatePromise('ballisticng gt (bngt)');
       expect(xml.length).to.equal(16355);
     });
-  
+
     it('should be able to load "Enai" template', async () => {
       const xml = await canLoadXmlFromRemoteTemplatePromise('enai example');
       expect(xml.length).to.equal(1937);
     });
-  
+
     it('should be able to load "kyra/evy/victor/shroom" template', async () => {
       const xml = await canLoadXmlFromRemoteTemplatePromise('kyra/evy/victor/shroom example');
       expect(xml.length).to.equal(2504);
     });
-  
+
     it('should be able to load "wipeout64 race challenges" template', async () => {
       const xml = await canLoadXmlFromRemoteTemplatePromise('wipeout64 race challenges');
       expect(xml.length).to.equal(4372);
     });
-  
+
     it('should be able to load "wipeout64 time trial challenges" template', async () => {
       const xml = await canLoadXmlFromRemoteTemplatePromise('wipeout64 time trial challenges');
       expect(xml.length).to.equal(4561);
     });
   });
 
-  describe('loading raw xml', function() {
+  describe('loading raw xml', function () {
     beforeEach(() => browser.get(targetHtmlUri));
 
     it('should be able to load raw xml', async () => {
@@ -88,77 +88,77 @@ describe('bng campaign xml generator', function() {
     });
     */
 
-    /* it('allow barracuda ship', async () => {});
+  /* it('allow barracuda ship', async () => {});
 
-    it('sample campaign vids', async () => {});
+  it('sample campaign vids', async () => {});
 
-    it('add event group', async () => {});
+  it('add event group', async () => {});
 
-    it('add event', async () => {});
+  it('add event', async () => {});
 
-    it('add track', async () => {});
+  it('add track', async () => {});
 
-    it('event name', async () => {});
+  it('event name', async () => {});
 
-    it('speed class', async () => {});
+  it('speed class', async () => {});
 
-    it('ai difficulty', async () => {});
+  it('ai difficulty', async () => {});
 
-    it('game mode', async () => {});
+  it('game mode', async () => {});
 
-    it('bronze medal threshold', async () => {});
+  it('bronze medal threshold', async () => {});
 
-    it('silver medal threshold', async () => {});
+  it('silver medal threshold', async () => {});
 
-    it('gold medal threshold', async () => {});
+  it('gold medal threshold', async () => {});
 
-    it('platinum medal threshold', async () => {});
+  it('platinum medal threshold', async () => {});
 
-    it('points required to unlock event group', async () => {});
+  it('points required to unlock event group', async () => {});
 
-    it('ai ships', async () => {});
+  it('ai ships', async () => {});
 
-    it('weapons', async () => {});
+  it('weapons', async () => {});
 
-    it('ship handling', async () => {});
+  it('ship handling', async () => {});
 
-    it('hardcore mode', async () => {});
+  it('hardcore mode', async () => {});
 
-    it('mirror all tracks in this event', async () => {});
+  it('mirror all tracks in this event', async () => {});
 
-    it('easy score', async () => {});
+  it('easy score', async () => {});
 
-    it('hard score', async () => {});
+  it('hard score', async () => {});
 
-    it('extra laps', async () => {});
+  it('extra laps', async () => {});
 
-    it('custom ai speed multiplier', async () => {});
+  it('custom ai speed multiplier', async () => {});
 
-    it('use the above custom ai speed multiplier', async () => {});
+  it('use the above custom ai speed multiplier', async () => {});
 
-    it('force player ship', async () => {});
+  it('force player ship', async () => {});
 
-    it('force ai ships to be same as player ship', async () => {});
+  it('force ai ships to be same as player ship', async () => {});
 
-    it('forced ship (only used if 1 of the above 2 options are set to "Yes")', async () => {});
+  it('forced ship (only used if 1 of the above 2 options are set to "Yes")', async () => {});
 
-    it('hide/show all event sections', async () => {});
+  it('hide/show all event sections', async () => {});
 
-    it('hide/show tracks', async () => {});
+  it('hide/show tracks', async () => {});
 
-    it('hide/show basic options', async () => {});
+  it('hide/show basic options', async () => {});
 
-    it('hide/show intermediate options', async () => {});
+  it('hide/show intermediate options', async () => {});
 
-    it('hide/show advanced options', async () => {});
+  it('hide/show advanced options', async () => {});
 
-    it('remove track', async () => {});
+  it('remove track', async () => {});
 
-    it('remove event', async () => {});
+  it('remove event', async () => {});
 
-    it('remove event group', async () => {});
-  });
-  */
+  it('remove event group', async () => {});
+});
+*/
 
   after(() => browser.quit());
 });
@@ -182,8 +182,7 @@ function convertBngCampaignXmlToXmlDocPromise(str) {
       if (err) {
         reject(err);
       }
-      else
-      {
+      else {
         resolve(xmlDoc);
       }
     });
@@ -224,7 +223,7 @@ function canLoadRawXmlPromise(rawXml) {
   return new Promise((resolve, reject) => {
     let prevXml = null;
     browser.findElement(by.id('campaign-xml')).getAttribute('value').then(xml => prevXml = xml);
-    browser.findElement(by.className('load-raw-xml')).sendKeys(rawXml + webdriver.Key.CONTROL+ "a" + "x" + "v")
+    browser.findElement(by.className('load-raw-xml')).sendKeys(rawXml + webdriver.Key.CONTROL + "a" + "x" + "v")
       .then(waitUntilPopupLoadElementRemoved)
       .then(() => waitUntilTemplateXmlLoadedPromise(prevXml))
       .then(() => browser.findElement(by.id('campaign-xml')).getAttribute('value'))
